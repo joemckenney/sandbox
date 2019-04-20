6dc9b7306c295e4d7fc3e4c0c21d99c5558eb58a
joemckenney <j.mckenney00@gmail.com>
2019-04-20 14:58:58 -0700

    Cleanup

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -5,19 +5,10 @@ import styles from './layout.css';
 
 function Layout({
   children,
-  contentRef,
   inset,
-  insetHorizontal,
-  insetVertical,
   overflow,
-  overflowX,
-  overflowY,
-  primaryAlignment,
-  secondaryAlignment,
   spacing,
   type,
-  size,
-  span,
   initial,
   static: _static,
   auto,
@@ -28,20 +19,11 @@ function Layout({
   return (
     <div
       {...attrs}
-      ref={contentRef}
       className={`layout ${styles.layout}`}
       data-flex={initial ? 'initial' : _static ? 'static' : 'auto'}
       data-inset={inset}
-      data-inset-horizontal={insetHorizontal}
-      data-inset-vertical={insetVertical}
       data-overflow={overflow}
-      data-overflow-x={overflowX}
-      data-overflow-y={overflowY}
-      data-primary-alignment={primaryAlignment}
-      data-secondary-alignment={secondaryAlignment}
-      data-size={size}
       data-spacing={spacing}
-      data-span={span}
       data-type={type}
     >
       {children}
@@ -54,13 +36,7 @@ Layout.propTypes = {
   auto: PropTypes.bool,
   initial: PropTypes.bool,
   inset: PropTypes.string,
-  insetHorizontal: PropTypes.string,
-  insetVertical: PropTypes.string,
   overflow: PropTypes.oneOf(['auto', 'hidden', 'overlay', 'visible']),
-  overflowX: PropTypes.oneOf(['auto', 'hidden', 'overlay', 'visible']),
-  overflowY: PropTypes.oneOf(['auto', 'hidden', 'overlay', 'visible']),
-  primaryAlignment: PropTypes.oneOf(['start', 'center', 'end']),
-  secondaryAlignment: PropTypes.oneOf(['start', 'center', 'end']),
   spacing: PropTypes.string,
   static: PropTypes.bool,
   type: PropTypes.oneOf(['vertical', 'horizontal'])
